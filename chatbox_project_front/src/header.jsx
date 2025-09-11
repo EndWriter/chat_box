9a64340a430d348ff6ced3fa9a5759008c80ee8d
Theosch16 <schmitt1690@gmail.com>
2025-09-11 08:58:30 +0200

    Ajout de Bootstrap et Feather-icons, page d'accueil

--- chatbox_project_front/src/header.jsx
+++ chatbox_project_front/src/header.jsx
@@ -1,32 +1,32 @@
-// import { useEffect } from "react";
-// import feather from "feather-icons";
+import { useEffect } from "react";
+import feather from "feather-icons";
 
-// // On exporte les données du header en le mettant dans un .jsx que l'on peut réutiliser plus tard
+// On exporte les données du header en le mettant dans un .jsx que l'on peut réutiliser plus tard
 
-// export default function Header(){
+export default function Header(){
 
-// useEffect(() => {
-//     feather.replace();
-//   }, []);
-// return (
-// <header data-bs-theme="dark">
-//     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-//         <div className="container-fluid">
-//             <a className="navbar-brand" href="#">Shoes King</a>
-//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarCollapse">
-//                 <ul className="navbar-nav me-auto mb-2 mb-md-0">
-//                 </ul>
-//                 <div className="speed-menu">
-//                     <a href=""><i data-feather="user"></i></a>
-//                     <a href=""><i data-feather="shopping-cart"></i></a>
-//                     <a href=""><i data-feather="help-circle"></i></a>
-//                 </div>
-//             </div>
-//         </div>
-//     </nav>
-// </header>
-// )
-// }
+useEffect(() => {
+    feather.replace();
+  }, []);
+return (
+<header data-bs-theme="dark">
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Shoes King</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                </ul>
+                <div className="speed-menu">
+                    <a href=""><i data-feather="user"></i></a>
+                    <a href=""><i data-feather="shopping-cart"></i></a>
+                    <a href=""><i data-feather="help-circle"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+)
+}
