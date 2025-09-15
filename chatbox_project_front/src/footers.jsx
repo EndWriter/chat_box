44c13f15cf617b28c2f39abf5f2593e13296f3e0
Theosch16 <schmitt1690@gmail.com>
2025-09-15 11:58:59 +0200

    Adding the homepage

--- chatbox_project_front/src/footers.jsx
+++ chatbox_project_front/src/footers.jsx
@@ -1,11 +1,12 @@
 import './assets/css/style.css'
 import './assets/css/carousel.css'
 import './assets/css/custom.css'
+import {Container} from "react-bootstrap"
 
 export default function Footer(){
     return (
-    <footer class="container">
-        <p>&copy; 2017–2024 La Boutique Française <a href="#">CGV</a> &middot; <a href="#">CGU</a></p>
+    <footer className="bg-dark text-white py-3">
+            <p>&copy; 2017–2025 Shoes King <a href="#">CGV</a> &middot; <a href="#">CGU</a></p>
     </footer>
     )
 }
