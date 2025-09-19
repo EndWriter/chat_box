9c22b5c7e0fa4baa39a66c9725dfcfd23ceb549f
Theosch16 <schmitt1690@gmail.com>
2025-09-19 10:53:00 +0200

    Homepage correction

--- chatbox_project_front/src/carousel.jsx
+++ chatbox_project_front/src/carousel.jsx
@@ -9,15 +9,13 @@ export default function Carousel(){
                 className="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"
                 aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
       </div>
 
       <div className="carousel-inner">
         <div className="carousel-item active">
           <img src={chaussure} className="d-block w-100" alt="Chaussure du magasin" />
           <div className="container">
-            <div className="carousel-caption text-start">
+            <div className="carousel-caption">
               <h1>Promotion jusqu'à -50%</h1>
               <p className="opacity-75">Votre première paire achetée à -25% et la deuxième à -50% !</p>
               <p><a className="btn btn-lg btn-primary" href="#">Inscrivez-vous</a></p>
@@ -26,25 +24,12 @@ export default function Carousel(){
         </div>
 
         <div className="carousel-item">
-          <img src={chaussure} className="d-block w-100" alt="Chaussure du magasin" />
+          <img src={chaussures} className="d-block w-100" alt="Chaussure du magasin" />
           <div className="container">
             <div className="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Some representative placeholder content for the second slide.</p>
-              <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-
-        <div className="carousel-item">
-          <svg className="bd-placeholder-img w-100 h-100" xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-          </svg>
-          <div className="container">
-            <div className="carousel-caption text-end">
-              <h1>One more for good measure.</h1>
-              <p>Some representative placeholder content for the third slide.</p>
-              <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+              <h1>Promotion jusqu'à -50%</h1>
+              <p>Votre première paire achetée à -25% et la deuxième à -50% !</p>
+              <p><a className="btn btn-lg btn-primary" href="#">Inscrivez-vous</a></p>
             </div>
           </div>
         </div>
